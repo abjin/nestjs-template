1cefbbfe1d428d5f83d50351704e307d90d1d973
김기진 <abjin69@gmail.com>
2025-02-06 22:41:35 +0900

    Remove DbModule import from app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
-import { DbModule } from 'db/db.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     { ...HttpModule.register({}), global: true },
-    DbModule,
   ],
   controllers: [AppController],
 })
